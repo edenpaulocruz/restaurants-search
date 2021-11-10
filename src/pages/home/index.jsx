b8913388dacc93272da4ec76367688f04f4e0a46
edenpaulocruz <edenpaulo@gmail.com>
2021-11-10 20:22:22 -0300

    Adição do logo, caixa de pesquisa e delimitação da área futura do mapa

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField, { Input } from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
+import logo from '../../assets/logo.svg';
+import { Container, Search, Logo, Wrapper, Map } from './styles';
 
-import { Container } from './styles';
+const Home = () => {
+  const [inputValue, setInputValue] = useState('');
 
-const Home = () => <Container>Hello World!</Container>;
+  return (
+    <Wrapper>
+      <Container>
+        <Search>
+          <Logo src={logo} alt="Logo do Restaurant Finder" />
+          <TextField
+            label="Pesquisar restaurantes"
+            outlined
+            trailingIcon={<MaterialIcon role="button" icon="search" />}>
+            <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+          </TextField>
+        </Search>
+      </Container>
+      <Map />
+    </Wrapper>
+  );
+};
 
 export default Home;
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,5 +1,31 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  background-color: ${(props) => props.theme.colors.primary};
+export const Wrapper = styled.div`
+  display: flex;
+`;
+
+export const Container = styled.aside`
+  background-color: ${(props) => props.theme.colors.background};
+  height: 100vh;
+  overflow-y: auto;
+  width: 360px;
+`;
+
+export const Search = styled.section`
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 1rem;
+`;
+
+export const Logo = styled.img`
+  align-self: center;
+  margin-bottom: 1rem;
+  width: 60%;
+`;
+
+export const Map = styled.div`
+  background-color: darkgray;
+  width: calc(100% - 360px)
 `;
